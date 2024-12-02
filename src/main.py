9c88f9f21d47a6be6f10fe20d4047241f9d97f21
Jabuf <jason.buffet@gmail.com>
2024-12-02 20:11:22 +0100

    add proper logging and clean main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
+import logging
 import os
-from typing import Union
 
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from src.api import router
+from src.api.routes import router
 from src.config.urls import API_VERSION
 from src.services.github import check_github_connection
 
 # Load environment variables from .env
 load_dotenv()
+
+log_level = os.getenv("LOG_LEVEL", logging.DEBUG)
+
+logging.basicConfig(
+    level=log_level,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+logger = logging.getLogger('uvicorn.error')
+logger.info("Log level : " + log_level)
 
 app = FastAPI()
 
@@ -19,17 +28,13 @@ check_github_connection()
 
 app.include_router(router, prefix=API_VERSION)
 
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"Mergify Stagazer": "A technical showcase project developed as part of the hiring process at Mergify"}
 
 
 if __name__ == "__main__":
     host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=host, port=port, log_level=log_level)
