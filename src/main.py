1ebedfae5494dfda2911cb7501be9d3970b44313
Jabuf <jason.buffet@gmail.com>
2024-12-02 13:28:49 +0100

    initialize fastapi
    While you don't want to commit .env, it's still useful to have a file with all up-to-date keys and default values

--- src/main.py
+++ src/main.py
@@ -1,2 +1,24 @@
+from typing import Union
+from fastapi import FastAPI
+import uvicorn
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
 if __name__ == "__main__":
-    print("Empty skeleton for now")
+    host = os.getenv("HOST", "127.0.0.1")
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host=host, port=port)
